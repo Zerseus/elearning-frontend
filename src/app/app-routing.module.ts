77f6233719050d504db1cb9fb3807e86560d1c22
Clyde Noronha <clyde_noronha@persistent.co.in>
2021-01-17 18:07:40 +0530

    added proper structure, need to commit to Bhanvi's repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,68 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {RestApiComponent} from "./restapi/restapi.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {InstructorDashboardComponent} from './instructor/instructor-dashboard/instructor-dashboard.component';
+import {InstructorDetailsComponent} from './instructor/instructor-details/instructor-details.component'
+import {InstructorUpdateDetailsComponent} from './instructor/instructor-update-details/instructor-update-details.component'
+import {StudentDashboardComponent} from './student/student-dashboard/student-dashboard.component';
+import {StudentUpdateDetailsComponent} from './student/student-update-details/student-update-details.component';
+import {CourseDetailsComponent} from './course/course-details/course-details.component';
+import {CourseContentListComponent} from './course/course-content-list/course-content-list.component';
+import {CourseDashboardComponent} from './course/course-dashboard/course-dashboard.component';
+import {CourseListComponent} from './course/course-list/course-list.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},  
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'restapi', component: RestApiComponent },
+  {
+    path:'instructor-dashboard',
+    component: InstructorDashboardComponent
+  },
+  {
+    path:'instructor-details',
+    component: InstructorDetailsComponent
+  },
+  {
+    path:'instructor-update-details',
+    component: InstructorUpdateDetailsComponent
+  },
+  {
+    path:'student-dashboard',
+    component: StudentDashboardComponent
+  },
+  {
+    path:'student-update-details',
+    component: StudentUpdateDetailsComponent
+  },
+  {
+    path:'course-list',
+    component: CourseListComponent
+  },
+  {
+    path:'course-dashboard',
+    component: CourseDashboardComponent
+  },
+  {
+    path:'course-content-list',
+    component: CourseContentListComponent
+  },
+  {
+    path:'course-details',
+    component: CourseDetailsComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
